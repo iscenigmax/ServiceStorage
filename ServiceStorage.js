5cc68f6a1732ffbdb9b12a6e31c23fac3546ece0
Carlos A. Sanchez <ing.casr@gmail.com>
2015-03-12 20:41:45 -0500

    Configuration for sheema

--- ServiceStorage.js
+++ ServiceStorage.js
@@ -1,64 +1,67 @@
 function ServiceStorage(opt) {
 	try {
-	    if (window.localStorage.getItem) {
-	        this.Storage = window.localStorage;
-	    }
+		if (window.localStorage.getItem) {
+			this.Storage = window.localStorage;
+		}
 	} catch(e) {
-	   this.Storage = {};
-	   console.log('El navegador no soporta Storage!!!');
-	    
+		this.Storage = {};
+		console.log("The browser does not support Storage!!!");
 	}
 
-   	this.domainname = window.location.hostname;
-   	this.doc = $(document);
+	this.domainname = window.location.hostname;
+	this.doc = $(document);
 
-   	$.extend(this, opt);
+	$.extend(this, opt);
 }
-/*sessionStorage*/
-ServiceStorage.prototype.setSession = function (key, value) {
+
+/* sessionStorage */
+ServiceStorage.prototype.setSession = function(key, value) {
 	window.sessionStorage.setItem(key, JSON.stringify(value));
 };
 
-ServiceStorage.prototype.getSession = function (key) {
+ServiceStorage.prototype.getSession = function(key) {
 	return JSON.parse(window.sessionStorage.getItem(key));
 };
 
-ServiceStorage.prototype.removeSession = function (key) {
+ServiceStorage.prototype.removeSession = function(key) {
 	window.sessionStorage.removeItem(key);
 };
 
-ServiceStorage.prototype.clearSession = function () {
+ServiceStorage.prototype.clearSession = function() {
 	window.sessionStorage.clear();
 };
 
-ServiceStorage.prototype.setLocalAutoSave = function (key,control) {
+ServiceStorage.prototype.setLocalAutoSave = function(key, control) {
 	setInterval(function() {
-    	window.sessionStorage.setItem(key, JSON.stringify(document.getElementById(control).value));
-	}, 1000*60);
+		window.sessionStorage.setItem(key, JSON.stringify(document.getElementById(control).value));
+	}, 1000 * 60);
 };
+/* sessionStorage */
 
-/*localStorage*/
-ServiceStorage.prototype.setLocal = function (key, value) {
+/* localStorage */
+ServiceStorage.prototype.setLocal = function(key, value) {
 	window.localStorage[this.domainname].setItem(key, JSON.stringify(value));
 };
 
-ServiceStorage.prototype.getLocal = function (key) {
+ServiceStorage.prototype.getLocal = function(key) {
 	return JSON.parse(window.localStorage[this.domainname].getItem(key));
 };
 
-ServiceStorage.prototype.removeLocal = function (key) {
+ServiceStorage.prototype.removeLocal = function(key) {
 	window.localStorage[this.domainname].removeItem(key);
 };
 
-ServiceStorage.prototype.clearLocal = function () {
+ServiceStorage.prototype.clearLocal = function() {
 	window.localStorage[this.domainname].clear();
 };
+/* localStorage */
 
-/*cache*/
-ServiceStorage.prototype.setCache = function (data) {
+/* cache */
+ServiceStorage.prototype.setCache = function(data) {
 	this.doc.data("cache", data);
 };
 
-ServiceStorage.prototype.getCache = function (data) {
+ServiceStorage.prototype.getCache = function(data) {
 	return this.doc.data("cache");
 };
+/* cache */
